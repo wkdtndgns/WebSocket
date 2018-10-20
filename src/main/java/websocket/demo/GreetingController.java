8c7e887b8679523692d80e8c6c0466e939d31401
wkdtndgns <wkdtndgns@naver.com>
2018-10-20 19:17:02 +0900

    react socket 적용중

--- src/main/java/websocket/demo/GreetingController.java
+++ src/main/java/websocket/demo/GreetingController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
