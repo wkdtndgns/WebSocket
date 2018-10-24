82e3707c3c7e66e8b0490de21c8c13697aaa92ed
wkdtndgns <wkdtndgns@naver.com>
2018-10-25 00:14:37 +0900

    통신 성공

--- src/main/java/websocket/demo/WebSocketConfig.java
+++ src/main/java/websocket/demo/WebSocketConfig.java
@@ -6,9 +6,19 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private static Set<Session> clients = Collections
+            .synchronizedSet(new HashSet<Session>());
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -19,7 +29,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("*").withSockJS();
     }
-
 
 
 }
